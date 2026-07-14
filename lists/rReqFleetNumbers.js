99a10d83767b554b70dfa4be3381f35c2ececaab
ross4122 <48660479+ross4122@users.noreply.github.com>
2026-07-14 10:04:49 +0100

    Update rReqFleetNumbers.js

--- lists/rReqFleetNumbers.js
+++ lists/rReqFleetNumbers.js
@@ -4,7 +4,7 @@ const rReqIconFleetNumbers = new Set([
 	"first|35135",
 	"first|37498",
    // STAGECOACH JUL 26
-    "stagecoach|15695",
+    "",
     "stagecoach|54305",
     "stagecoach|54306",
     "stagecoach|54407"
